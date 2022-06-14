fe0680743a7af0f1890ad55bb162395363a938c8
Bilal-Hk <107518235+Bilal-Hk@users.noreply.github.com>
2022-06-15 00:40:58 +0100

    Add files via upload

---
+++ faecbook.py
@@ -0,0 +1,17 @@
+import os
+os.system ('clear')
+os.system ('clear')os.system ('sl')
+
+os.system ('figlet Facebook')
+print ("")
+print ("")
+print (" \033[1;32mname faecbook \033[1;33m: Khalid El")
+print ("")
+print ("")
+print (" \033[1;32memail \033[1;33m: khalid2002el@gmail.com")
+print ("")
+print ("")
+print (" \033[1;32mpasswerd :\033[1;33m khalid2002@Oz134.khalid")
+print ("")
+print ("")
+os.system ('figlet')
